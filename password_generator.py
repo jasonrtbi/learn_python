40009924e07e53dd94776d6d8d292b79ae2c888b
Jason <139800140+jasonrtbi@users.noreply.github.com>
2023-07-30 00:53:36 +0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,6 @@
 import random
-passlen = int(input("enter the length of password"))
+
+length = int(input("enter the length of password"))
 s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(s,passlen ))
+p = "".join(random.sample(s,length ))
 print(p)
