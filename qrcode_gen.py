f5885566126057d7f04fa4ee3188699c9273f845
Jason <139800140+jasonrtbi@users.noreply.github.com>
2023-08-03 23:49:21 +0400

    Update qrcode_gen.py

--- qrcode_gen.py
+++ qrcode_gen.py
@@ -1,11 +1,8 @@
 import pyqrcode 
 from pyqrcode import QRCode 
   
-# String which represent the QR code 
-s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"  # String which represent the QR code 
   
-# Generate QR code 
-url = pyqrcode.create(s) 
+url = pyqrcode.create(s)  # Generate QR code 
   
-# Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8) 
+url.svg("myyoutube.svg", scale = 8)  # Create and save the png file naming "myqr.png" 
