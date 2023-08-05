5887771b337563935b72717d996a09276ee094ae
Jason <139800140+jasonrtbi@users.noreply.github.com>
2023-08-05 23:26:04 +0400

    Update qrcode_gen.py

--- qrcode_gen.py
+++ qrcode_gen.py
@@ -1,8 +1,9 @@
 import pyqrcode 
 from pyqrcode import QRCode 
-  
-s = "https://www.google.com"  # String which represent the QR code 
-  
+
+print('Paste your link here')
+s = input()
+
 url = pyqrcode.create(s)  # Generate QR code 
   
 url.svg("myyoutube.svg", scale = 8)  # Create and save the png file naming "myqr.png" 
